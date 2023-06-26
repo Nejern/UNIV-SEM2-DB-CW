cc0cab6a98a606a0e47124664ceba510d594d8ef
Nejern <5qub6pfp@duck.com>
2023-06-26 19:54:10 +0300

    Updated functions

--- Completed/functions/all_funtions.sql
+++ Completed/functions/all_funtions.sql
@@ -1,3 +1,24 @@
+-- Таблица всех расходов конкретного отдела по годам и месяцам
+CREATE OR REPLACE FUNCTION department_expenses(depart_id INT)
+RETURNS TABLE (year DECIMAL, month DECIMAL, expenses DECIMAL)
+AS $$
+BEGIN
+	RETURN QUERY
+	SELECT
+		EXTRACT(YEAR FROM expenses.expense_date) AS year,
+		EXTRACT(MONTH FROM expenses.expense_date) AS month,
+		COALESCE(SUM(expenses.amount), 0) AS expenses
+	FROM expenses, staff
+	WHERE
+		expenses.staffer_id = staff.id
+		AND staff.department_id = depart_id
+	GROUP BY year, month
+	ORDER BY year, month;
+
+	RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
 -- Сумма всех расходов конкретного отдела
 CREATE OR REPLACE FUNCTION department_expenses_alltime(depart_id INT)
 RETURNS DECIMAL
@@ -34,27 +55,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Таблица всех расходов конкретного отдела по годам и месяцам
-CREATE OR REPLACE FUNCTION department_expenses(depart_id INT)
-RETURNS TABLE (year DECIMAL, month DECIMAL, expenses DECIMAL)
-AS $$
-BEGIN
-	RETURN QUERY
-	SELECT
-		EXTRACT(YEAR FROM expenses.expense_date) AS year,
-		EXTRACT(MONTH FROM expenses.expense_date) AS month,
-		COALESCE(SUM(expenses.amount), 0) AS expenses
-	FROM expenses, staff
-	WHERE
-		expenses.staffer_id = staff.id
-		AND staff.department_id = depart_id
-	GROUP BY year, month
-	ORDER BY year, month;
-
-	RETURN;
-END;
-$$ LANGUAGE plpgsql;
-
 -- Таблица всех расходов сотрудников
 DROP FUNCTION IF EXISTS staff_expenses();
 CREATE OR REPLACE FUNCTION staff_expenses()
@@ -73,5 +73,36 @@ BEGIN
 		,expenses.amount
 	FROM expenses, staff
 	WHERE expenses.staffer_id = staff.id;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Оставшаяся не потраченная сумма отдела
+CREATE OR REPLACE FUNCTION department_remaining_amount(depart_id INT)
+RETURNS DECIMAL
+AS $$
+DECLARE
+	remaining_amount DECIMAL;
+BEGIN
+	IF NOT EXISTS (SELECT 1 FROM departments WHERE id = depart_id) THEN
+		RAISE EXCEPTION 'Department does not exist';
+	END IF;
+	SELECT
+		COALESCE((
+			SELECT SUM(max_amount)
+			FROM departments_expense_types
+			WHERE
+				department_id = depart_id
+				AND datestamp <= NOW()
+		), 0) -
+		COALESCE((
+			SELECT SUM(amount)
+			FROM expenses, staff
+			WHERE
+				staff.department_id = depart_id
+				AND expenses.staffer_id = staff.id
+				AND expense_date <= NOW()
+		), 0) INTO remaining_amount;
+
+	RETURN remaining_amount;
 END;
 $$ LANGUAGE plpgsql;
